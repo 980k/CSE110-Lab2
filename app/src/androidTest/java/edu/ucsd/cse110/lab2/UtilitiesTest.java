7b20336b0504567f26cb7348d176306699c9dd83
980k <53587009+980k@users.noreply.github.com>
2023-01-17 17:43:55 -0800

    refactored trimDisplayStr method and added unit test

---
+++ app/src/androidTest/java/edu/ucsd/cse110/lab2/UtilitiesTest.java
@@ -0,0 +1,16 @@
+package edu.ucsd.cse110.lab2;
+
+import static org.junit.Assert.*;
+
+import android.speech.tts.UtteranceProgressListener;
+
+import org.junit.Test;
+
+public class UtilitiesTest {
+    @Test
+    public void test_trimDisplayStr() {
+        assertEquals("1.234", Utilities.trimDisplayStr("1.2340"));
+        assertEquals("13", Utilities.trimDisplayStr("13.0"));
+    }
+
+}
